12e927eec7fe5631b0714dac6cec0378aede155e
xfokiux xfokiux <xfokiuxxfokiux@Diegos-MacBook-Air.local>
2025-05-16 19:34:07 -0500

    responsive Intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -6,10 +6,10 @@ import wapp from "../../public/img/WhatsApp.svg";
 export default function Intro() {
   return (
     <section className="p-5">
-      <div>
+      <div className="flex flex-col items-center md:items-start">
         <h1 className="font-black text-5xl uppercase">Nagare Bjj</h1>
         <h4 className="text-red-700 text-2xl font-bold">El arte de fluir</h4>
-        <p className="text-sm mt-5">
+        <p className="text-sm mt-5 md:w-2xl">
           Te ofrecemos la mejor experiencia y un ambiente muy fraternal .
           Tenemos clases con los mejores profesores, capacitados para llevar tu
           proceso a otro nivel. Encontraras clases de JIU-JITSU Brasileño,
@@ -18,11 +18,10 @@ export default function Intro() {
       </div>
 
       <div>
-        <div>
-          <p className="mt-7 font-light">¿Estas en Bogotá?</p>
-        </div>
-
-        <div>
+        <div className="flex flex-col items-center md:items-start">
+          <div>
+            <p className="mt-7 font-light">¿Estas en Bogotá?</p>
+          </div>
           <Link href="https://wa.me/573505473752" target="_blank">
             <div className="w-40 flex gap-2 p-2 mt-2 bg-red-700 hover:bg-red-800 rounded-sm text-white font-bold cursor-pointer">
               <div>
