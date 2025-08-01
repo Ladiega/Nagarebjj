781a54dd05115cdc84dc76353941a6d6caa73feb
xfokiux xfokiux <xfokiuxxfokiux@192.168.1.12>
2025-08-01 16:57:06 -0500

    update photo and ubication url

--- src/components/Ubicacion.tsx
+++ src/components/Ubicacion.tsx
@@ -18,7 +18,7 @@ export default function Ubicacion() {
         </div>
         <div className="hover:bg-red-500/10">
           <Link
-            href="https://www.google.com/maps/dir/4.6591216,-74.0636358/Nagare+Brazilian+Jiujitsu+-+Defensa+Personal/@4.6625139,-74.0661493,16.76z/data=!4m10!4m9!1m1!4e1!1m5!1m1!1s0x8e3f9b78cff70663:0x1379954f6271a50c!2m2!1d-74.0655724!2d4.6642058!3e0?entry=ttu&g_ep=EgoyMDI1MDUwNy4wIKXMDSoASAFQAw%3D%3D"
+            href="https://www.google.com/maps/place/Nagare+Brazilian+Jiujitsu+-+Defensa+Personal/@4.6623744,-74.0628981,17z/data=!4m6!3m5!1s0x8e3f9b78cff70663:0x1379954f6271a50c!8m2!3d4.6623744!4d-74.0628981!16s%2Fg%2F11smvm52gs?entry=ttu&g_ep=EgoyMDI1MDcyOS4wIKXMDSoASAFQAw%3D%3D"
             target="_blank"
           >
             <Image src={ubicacion} alt="Ubicacion" className="" />
